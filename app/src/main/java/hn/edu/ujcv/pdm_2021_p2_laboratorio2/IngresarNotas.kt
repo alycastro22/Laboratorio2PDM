3a1984a1654027f7a1ee475d312c4a6228516c8f
stephany Carbajal <stephany.carbajal1@ujcv.edu.hn>
2021-03-07 01:44:06 -0600

    Codigo para volver al menú inicial desde todas las pantallas

--- app/src/main/java/hn/edu/ujcv/pdm_2021_p2_laboratorio2/IngresarNotas.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_p2_laboratorio2/IngresarNotas.kt
@@ -1,11 +1,18 @@
 package hn.edu.ujcv.pdm_2021_p2_laboratorio2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class IngresarNotas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ingresar_notas)
+    }
+
+    fun cambioPantalla(view: View) {
+        val cambio = Intent(this, MainActivity::class.java)
+        startActivity(cambio)
     }
 }
--- app/src/main/java/hn/edu/ujcv/pdm_2021_p2_laboratorio2/RegistrarAlumno.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_p2_laboratorio2/RegistrarAlumno.kt
@@ -1,11 +1,18 @@
 package hn.edu.ujcv.pdm_2021_p2_laboratorio2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class RegistrarAlumno : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrar_clase)
     }
+    fun cambioPantalla(view: View) {
+        val cambio = Intent(this, MainActivity::class.java)
+        startActivity(cambio)
+    }
+
 }
