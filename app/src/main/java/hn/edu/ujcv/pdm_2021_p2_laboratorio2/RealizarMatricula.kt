40183bb344edaaabd46531d9c287ff64001dae6a
aramel08 <78820331+aramel08@users.noreply.github.com>
2021-03-14 01:42:16 -0600

    Enviar matricula correo

--- app/src/main/java/hn/edu/ujcv/pdm_2021_p2_laboratorio2/RealizarMatricula.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_p2_laboratorio2/RealizarMatricula.kt
@@ -1,20 +1,56 @@
 package hn.edu.ujcv.pdm_2021_p2_laboratorio2
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_realizar_matricula.*
 import java.lang.StringBuilder
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.HashMap
 
 class RealizarMatricula : AppCompatActivity() {
     var valores : HashMap<Int,String> = hashMapOf()
     var numero = 0
+    var listItens = ArrayList<String>()
+    var adapter: ArrayAdapter<String>?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_realizar_matricula)
+        txtMatricular.setOnClickListener { guardarmatricula() }
 
+        findViewById<FloatingActionButton>(R.id.enviar).setOnClickListener { view ->
+            addListItem()
+            Snackbar.make(view, "Enviar matricula", Snackbar.LENGTH_LONG)
+                .setAction("Deshacer", deshacerOnClickListener).show()
+        }
+    }
+    var deshacerOnClickListener: View.OnClickListener = View.OnClickListener { view ->
+        listItens.removeAt(listItens.size -1)
+        adapter?.notifyDataSetChanged()
+        Snackbar.make(view, "Correo enciado cancelado", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
+    }
+    private fun addListItem(){
+        val correo: String=(txtcorreo.text.toString())
+        listItens.add(correo)
+        adapter?.notifyDataSetChanged()
+    }
+
+    override fun onStart(){
+        super.onStart()
+        adapter= ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            listItens)
+
+        lstView.adapter = adapter
     }
     fun cambioPantalla(view: View) {
         val cambio = Intent(this, MainActivity::class.java)
@@ -27,7 +63,20 @@ class RealizarMatricula : AppCompatActivity() {
             dato.append(txtAlumno.text.toString().trim()).append("|")
             dato.append(txtAsignatura.text.toString())
             valores.put(numero, dato.toString())
-            Toast.makeText(this, "Se matriculo con exito", Toast.LENGTH_LONG)
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Confirmación Recarga")
+            builder.setMessage("¿Desea enviar Matricula por correo?")
+            builder.setPositiveButton("Enviar", { dialogInterface: DialogInterface, i: Int ->
+                txtcorreo.visibility = View.VISIBLE
+                enviar.visibility = View.VISIBLE
+
+
+            })
+            builder.setNegativeButton("No enviar", { dialogInterface: DialogInterface, i: Int ->
+                Toast.makeText(this, "Se matriculo con exito", Toast.LENGTH_LONG)
+            })
+            builder.show()
+
 
         }else{
             if (txtAlumno.text.isEmpty()){
